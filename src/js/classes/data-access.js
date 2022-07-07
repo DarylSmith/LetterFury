23e5a6da73860f593d0bea851e529c7c553d1b13
Daryl Smith <daryl1976@gmail.com>
2022-07-06 21:36:26 -0400

    Removed tsc action and included compiled js files

---
+++ src/js/classes/data-access.js
@@ -0,0 +1,72 @@
+import { GroupGameFunction } from '../enums/group-game-function.js';
+export class DataAccess {
+    constructor() {
+        this._startGroupGameEndpoint = 'https://5c7skzyfq2.execute-api.us-east-1.amazonaws.com/default/create-group-game';
+        this._gameSocketEndpoint = 'wss://piz3bjhjvc.execute-api.us-east-1.amazonaws.com/Prod';
+    }
+    StartGroupGame(gameId, playerId) {
+        return fetch(`${this._startGroupGameEndpoint}`, {
+            mode: 'no-cors',
+            method: 'post',
+            headers: {},
+            body: JSON.stringify({ gameId, playerId })
+        });
+    }
+    InvokeSocketConnection(gameId, playerId) {
+        this._gameSocket = new WebSocket(`${this._gameSocketEndpoint}?game=${gameId}`);
+        this._gameSocket.onerror = (event) => {
+            let socketEvent = new CustomEvent("socketError");
+            document.dispatchEvent(socketEvent);
+        };
+        this._gameSocket.onopen = (event) => {
+            let socketEvent = new CustomEvent("socketOpen");
+            document.dispatchEvent(socketEvent);
+        };
+        this._gameSocket.onmessage = (event) => {
+            let payload = JSON.parse(event.data);
+            let socketEvent = new CustomEvent("socketEvent", { detail: payload });
+            document.dispatchEvent(socketEvent);
+        };
+        this._gameSocket.onopen = (event) => {
+            const startObj = {
+                action: 'sendmessage',
+                function: GroupGameFunction.PlayerName,
+                data: playerId,
+                game: gameId
+            };
+            this.SendGameMessage(startObj);
+        };
+    }
+    InvokeSocketGameStart(word, game) {
+        const startObj = {
+            action: 'sendmessage',
+            function: GroupGameFunction.GameStart,
+            data: word,
+            game: game
+        };
+        this.SendGameMessage(startObj);
+    }
+    InvokeSocketGameEnd(game) {
+        const startObj = {
+            action: 'sendmessage',
+            function: GroupGameFunction.GameEnd,
+            game: game
+        };
+        this.SendGameMessage(startObj);
+    }
+    InvokeSocketWordGuessed(word, playerName, game, score) {
+        const startObj = {
+            action: 'sendmessage',
+            function: GroupGameFunction.WordGuessed,
+            data: word,
+            game: game,
+            playerName: playerName,
+            score: score
+        };
+        this.SendGameMessage(startObj);
+    }
+    SendGameMessage(payloadObj) {
+        const payload = JSON.stringify(payloadObj);
+        this._gameSocket.send(payload);
+    }
+}
---
+++ src/js/enums/group-game-function.js
@@ -0,0 +1,7 @@
+export var GroupGameFunction;
+(function (GroupGameFunction) {
+    GroupGameFunction["PlayerName"] = "playerName";
+    GroupGameFunction["GameStart"] = "gameStart";
+    GroupGameFunction["WordGuessed"] = "wordGuessed";
+    GroupGameFunction["GameEnd"] = "gameEnded";
+})(GroupGameFunction || (GroupGameFunction = {}));
